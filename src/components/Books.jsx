7b9fde954ce57a8bd52489564aadf063b68d16e6
Joe Hsiao <joe.phsiao@gmail.com>
2025-03-06 16:19:53 -0800

    complete 8.19

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,14 +1,18 @@
 import { useQuery } from '@apollo/client'
 import { ALL_BOOKS } from "../queries"
 import PropTypes from 'prop-types'
+import { useState } from 'react'
 
 const Books = (props) => {
+  const [filterGenre, setFilterGenre] = useState('')
+
   const result = useQuery(ALL_BOOKS)
   if (result.loading) {
     return <div>Loading...</div>
   }
 
-  const books = result.data.allBooks
+  const books =
+    result.data.allBooks
 
   if (!props.show) {
     return null
@@ -25,15 +29,22 @@ const Books = (props) => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map((a) => (
-            <tr key={a.title}>
-              <td>{a.title}</td>
-              <td>{a.author.name}</td>
-              <td>{a.published}</td>
-            </tr>
-          ))}
+          {books
+            .filter(x => !filterGenre || x.genres.includes(filterGenre))
+            .map((a) => (
+              <tr key={a.title}>
+                <td>{a.title}</td>
+                <td>{a.author.name}</td>
+                <td>{a.published}</td>
+              </tr>
+            ))}
         </tbody>
       </table>
+      <button onClick={() => setFilterGenre('refactoring')}>refactoring</button>
+      <button onClick={() => setFilterGenre('agile')}>agile</button>
+      <button onClick={() => setFilterGenre('patterns')}>patterns</button>
+      <button onClick={() => setFilterGenre('design')}>design</button>
+      <button onClick={() => setFilterGenre('')}>all genres</button>
     </div>
   )
 }
